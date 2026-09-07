857eeca88f22aef584c92f9c612234fdf37378cf
majeed-ali <amajeedali0@gmail.com>
2026-09-07 21:19:41 +0530

    Sitemap updated

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,90 @@ const nextConfig = {
 	turbopack: {
 		root: projectRoot
 	},
+	async redirects() {
+		return [
+			{
+				source: '/youtube-video-downloader',
+				destination: '/en',
+				permanent: true
+			},
+			{
+				source: '/:locale/youtube-video-downloader',
+				destination: '/:locale',
+				permanent: true
+			},
+			{
+				source: '/video-downloader',
+				destination: '/en',
+				permanent: true
+			},
+			{
+				source: '/:locale/video-downloader',
+				destination: '/:locale',
+				permanent: true
+			},
+			{
+				source: '/dailymotion-video-downloader',
+				destination: '/en/daily-motion-downloader',
+				permanent: true
+			},
+			{
+				source: '/:locale/dailymotion-video-downloader',
+				destination: '/:locale/daily-motion-downloader',
+				permanent: true
+			},
+			{
+				source: '/facebook-video-downloader',
+				destination: '/en/facebook-downloader',
+				permanent: true
+			},
+			{
+				source: '/:locale/facebook-video-downloader',
+				destination: '/:locale/facebook-downloader',
+				permanent: true
+			},
+			{
+				source: '/vimeo-video-downloader',
+				destination: '/en/vimeo-downloader',
+				permanent: true
+			},
+			{
+				source: '/:locale/vimeo-video-downloader',
+				destination: '/:locale/vimeo-downloader',
+				permanent: true
+			},
+			{
+				source: '/instagram-video-downloader',
+				destination: '/en/instagram-downloader',
+				permanent: true
+			},
+			{
+				source: '/:locale/instagram-video-downloader',
+				destination: '/:locale/instagram-downloader',
+				permanent: true
+			},
+			{
+				source: '/privacy-policy',
+				destination: '/en',
+				permanent: true
+			},
+			{
+				source: '/:locale/privacy-policy',
+				destination: '/:locale',
+				permanent: true
+			},
+			{
+				source: '/terms-of-service',
+				destination: '/en',
+				permanent: true
+			},
+			{
+				source: '/:locale/terms-of-service',
+				destination: '/:locale',
+				permanent: true
+			}
+		];
+	},
 	async headers() {
 		return [
 			{
